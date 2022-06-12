72df442b6fed10498d27e38a84fb07450a9dabb6
Nicolás Mayora <nicolas.mayora@fing.edu.uy>
2022-06-12 09:42:01 -0300

    Initial dev files

---
+++ src/TypeChecker.hs
@@ -0,0 +1,23 @@
+-- LABORATORIO DE PROGRAMACION FUNCIONAL 2022
+-- MODULO DE CHEQUEO DE NOMBRES Y TIPOS
+
+module TypeChecker where
+
+import Syntax
+
+
+data Error = Duplicated      Name
+           | Undefined       Name
+           | Expected        Type Type
+
+instance Show Error where
+   show (Duplicated      n)  = "Duplicated definition: " ++ n
+   show (Undefined       n)  = "Undefined: " ++ n
+   show (Expected    ty ty') = "Expected: " ++ show ty
+                             ++ " Actual: " ++ show ty'
+
+type Env = [(Name, Type)]
+
+-- Implementar
+checkProgram :: Program -> [Error]
+checkProgram = undefined
