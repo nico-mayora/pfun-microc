7202fc8821c4969a0712e9b36e3b04e64c16915f
Nicolás Mayora <nicolas.mayora@xmartlabs.com>
2022-06-13 19:43:20 -0300

    CHECK TYPES ALMOST DONE

---
+++ Session.vim
@@ -0,0 +1,51 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd ~/Documents/Fing/fp/tarea/plog-microc
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+let s:shortmess_save = &shortmess
+set shortmess=aoO
+badd +39 src/Syntax.hs
+badd +94 src/TypeChecker.hs
+argglobal
+%argdel
+edit src/TypeChecker.hs
+argglobal
+balt src/Syntax.hs
+setlocal fdm=expr
+setlocal fde=nvim_treesitter#foldexpr()
+setlocal fmr={{{,}}}
+setlocal fdi=#
+setlocal fdl=0
+setlocal fml=1
+setlocal fdn=20
+setlocal nofen
+let s:l = 49 - ((34 * winheight(0) + 19) / 38)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 49
+normal! 0
+tabnext 1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20
+let &shortmess = s:shortmess_save
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
+let g:this_session = v:this_session
+let g:this_obsession = v:this_session
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -3,49 +3,56 @@
 
 module TypeChecker where
 
+import Distribution.TestSuite (Result (Error))
 import Syntax
-import Distribution.TestSuite (Result(Error))
 import System.Posix.Internals (newFilePath)
 
-
-data Error = Duplicated      Name
-           | Undefined       Name
-           | Expected        Type Type
+data Error
+  = Duplicated Name
+  | Undefined Name
+  | Expected Type Type
 
 instance Show Error where
-   show (Duplicated      n)  = "Duplicated definition: " ++ n
-   show (Undefined       n)  = "Undefined: " ++ n
-   show (Expected    ty ty') = "Expected: " ++ show ty
-                             ++ " Actual: " ++ show ty'
+  show (Duplicated n) = "Duplicated definition: " ++ n
+  show (Undefined n) = "Undefined: " ++ n
+  show (Expected ty ty') =
+    "Expected: " ++ show ty
+      ++ " Actual: "
+      ++ show ty'
 
 type Env = [(Name, Type)]
 
-getProg :: Either String Program -> Program 
+getProg :: Either String Program -> Program
 getProg (Right p) = p
 getProg (Left _) = Program []
 
 -- Implementar
 checkProgram :: Program -> [Error]
-checkProgram prg =   if null nameErrors
-                        then typeErrors
-                        else nameErrors
-                     where nameErrors = checkNames prg []
-                           typeErrors = checkTypes prg
+checkProgram prg =
+  if null nameErrors
+    then typeErrors
+    else nameErrors
+  where
+    nameErrors = checkNames prg []
+    typeErrors = checkTypes prg
 
 type EnvError = (Env, [Error])
 
 checkNames :: Program -> Env -> [Error]
 checkNames (Program mb) env = snd $ foldl processLine (env, []) mb
-   where processLine envErrors (Com stmt) = (fst envErrors, checkNamesStmt envErrors stmt)
-         processLine envErrors (Decl varDecl) = checkNamesDecl envErrors varDecl
+  where
+    processLine envErrors (Com stmt) = (fst envErrors, checkNamesStmt envErrors stmt)
+    processLine envErrors (Decl varDecl) = checkNamesDecl envErrors varDecl
 
 checkNamesStmt :: EnvError -> Stmt -> [Error]
 checkNamesStmt (env, errors) (StmtExpr expr) = errors ++ checkExpr env expr
 checkNamesStmt (env, errors) (If expr body1 body2) = checkExpr env expr ++ checkNamesBody1 ++ checkNamesBody2
-   where checkNamesBody1 = foldl (\x -> checkNamesStmt (env, x)) [] body1
-         checkNamesBody2 = foldl (\x -> checkNamesStmt (env, x)) [] body2
+  where
+    checkNamesBody1 = foldl (\x -> checkNamesStmt (env, x)) [] body1
+    checkNamesBody2 = foldl (\x -> checkNamesStmt (env, x)) [] body2
 checkNamesStmt (env, errors) (While expr body) = checkExpr env expr ++ checkNamesBody
-   where checkNamesBody = foldl (\x -> checkNamesStmt (env, x)) [] body
+  where
+    checkNamesBody = foldl (\x -> checkNamesStmt (env, x)) [] body
 checkNamesStmt (env, errors) (PutChar expr) = errors ++ checkExpr env expr
 
 checkExpr :: Env -> Expr -> [Error]
@@ -57,14 +64,61 @@ checkExpr _ _ = []
 
 checkNamesDecl :: EnvError -> VarDef -> EnvError
 checkNamesDecl (env, errors) (VarDef varType name) =
-   if null $ usedInEnv env name
-      then ((name, varType):env, errors)
-      else (env, errors++[Duplicated name])
+  if null $ usedInEnv env name
+    then ((name, varType) : env, errors)
+    else (env, errors ++ [Duplicated name])
 
 usedInEnv :: Env -> String -> Env
 usedInEnv env name = filter (\x -> fst x == name) env
 
+type TypeError = (Type, [Error])
 
+checkTypes :: Env -> Program -> [Error]
+checkTypes env (Program mb) = snd $ foldl processLine (env, []) mb
+  where
+    processLine envErrors (Com stmt) = (fst envErrors, snd $ processStmt envErrors stmt)
+    processLine envErrors _ = envErrors
 
-checkTypes :: Program -> [Error]
-checkTypes _ = []
+errBody :: Env -> Body -> [Error]
+errBody env stmts = snd $ foldl processStmt (env, []) stmts
+
+processStmt :: EnvError -> Stmt -> EnvError
+processStmt (env, errs) (StmtExpr expr) = (env, errs ++ snd (expType env expr))
+processStmt (env, errs) (If expr body1 body2) =
+  let expTup = expType env expr
+   in (env, errs ++ snd expTup ++ [Expected TyInt TyChar | fst expTup /= TyInt] ++ errBody env body1 ++ errBody env body2)
+processStmt (env, errs) (While expr body) =
+  let expTup = expType env expr
+   in (env, errs ++ snd expTup ++ [Expected TyInt TyChar | fst expTup /= TyInt] ++ errBody env body)
+processStmt (env, errs) (PutChar expr) =
+  let expTup = expType env expr
+   in (env, errs ++ snd expTup ++ [Expected TyChar TyInt | fst expTup /= TyInt])
+
+expType :: Env -> Expr -> TypeError
+expType env (Var name) = (snd . head $ filter (\x -> fst x == name) env, [])
+expType env (CharLit _) = (TyChar, [])
+expType env (NatLit _) = (TyInt, [])
+expType env GetChar = (TyChar, [])
+expType env (Unary _ exp) =
+  let expTup = expType env exp
+   in (TyInt, snd expTup ++ [Expected TyInt TyChar | fst expTup /= TyInt])
+expType env (Binary bop exp1 exp2) -- TODO: revisar cantidad & orden de errores
+  | bop == Less || bop == Equ =
+    let err = case (fst expTup1, fst expTup2) of
+          (TyInt, TyChar) -> [Expected TyInt TyChar]
+          (TyChar, TyInt) -> [Expected TyChar TyInt]
+          _ -> []
+     in (fst expTup1, snd expTup1 ++ snd expTup2 ++ err)
+  | otherwise =
+    let err = case (fst expTup1, fst expTup2) of
+          (TyInt, TyInt) -> []
+          (TyChar, TyChar) -> [Expected TyInt TyChar, Expected TyInt TyChar]
+          _ -> [Expected TyInt TyChar]
+     in (TyInt, snd expTup1 ++ snd expTup2 ++ err)
+  where
+    expTup1 = expType env exp1
+    expTup2 = expType env exp2
+expType env (Assign name exp) =
+  let nameType = snd . head $ filter (\x -> fst x == name) env
+      expTup = expType env exp
+   in (nameType, [Expected nameType $ fst expTup | nameType /= fst expTup])
